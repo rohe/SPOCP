f4d0a183f0c2ec23da1a719f1d917fdcede5ffc3
Roland Hedberg <roland@catalogix.se>
2004-01-07 17:38:13 +0000

    Change to comply with changes in function naming in the spocp client library
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@18 64b275d1-5406-0410-b6a0-e042953264b1

--- backends/spocp/spocp.c
+++ backends/spocp/spocp.c
@@ -22,8 +22,8 @@ int P_spocp_close( void *vp )
 
   spocp = (SPOCP *) vp ;
 
-  spocp_send_logout( spocp ) ;
-  close_connection( spocp ) ;
+  spocpc_send_logout( spocp ) ;
+  spocpc_close( spocp ) ;
   free_spocp( spocp ) ;
 
   return 1 ;
@@ -48,7 +48,7 @@ int spocp_test( octet_t *arg, becpool_t *bcp, octet_t *blob )
   traceLog("Spocp query to %s", server ) ;
 
   if( bcp == 0 || ( bc = becon_get( "spocp", server, bcp )) == 0 ) {
-    if(( spocp = open_con( server )) == 0 ) {
+    if(( spocp = spocpc_open( server )) == 0 ) {
       traceLog("Couldn't open connection") ;
       r = SPOCP_UNAVAILABLE ;
       goto done ;
@@ -59,8 +59,8 @@ int spocp_test( octet_t *arg, becpool_t *bcp, octet_t *blob )
   else {
     spocp = (SPOCP *) bc->con ;
     /* how to check that the server is still there ?  Except sending a dummy query ? */
-    if(( r = spocp_send_query( spocp, "", "(5:XxXxX)", 0 )) == SPOCP_UNAVAILABLE ) {
-      if( reopen_con( spocp  ) == FALSE ) {
+    if(( r = spocpc_send_query( spocp, "", "(5:XxXxX)", 0 )) == SPOCP_UNAVAILABLE ) {
+      if( spocpc_reopen( spocp  ) == FALSE ) {
         becon_rm( bcp, bc ) ;
         free( server ) ;
         oct_freearr( argv ) ;
@@ -80,7 +80,7 @@ int spocp_test( octet_t *arg, becpool_t *bcp, octet_t *blob )
   query = oct2strdup( argv[2], 0 ) ;
   memset( &on, 0, sizeof( octnode_t )) ;
 
-  if(( r = spocp_send_query( spocp, path, query, &on )) == SPOCP_SUCCESS ) {
+  if(( r = spocpc_send_query( spocp, path, query, &on )) == SPOCP_SUCCESS ) {
     if( on.oct.size ) {
       octmove( blob, &on.oct ) ;
       if( on.next ) octnode_free( on.next ) ;
