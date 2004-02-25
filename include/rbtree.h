45c2039bdae45265633ee1f9681f612e36f4887b
Roland Hedberg <roland@catalogix.se>
2004-02-25 13:55:48 +0000

    Changed storage struct
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@61 64b275d1-5406-0410-b6a0-e042953264b1

--- include/rbtree.h
+++ include/rbtree.h
@@ -1,11 +1,11 @@
-typedef char * item_t ;
+#ifndef __RBTREE_H_
+#define __RBTREE_H_
 
-typedef unsigned int Key ;
+#include <struct.h>
+#include <basefn.h>
+#include <varr.h>
 
-typedef Key (kfunc)( item_t a ) ;
-typedef void (ffunc)( item_t a ) ;
-typedef item_t (dfunc)( item_t a ) ;
-typedef int (cmpfn)( Key a, Key b ) ;
+struct _varr ;
 
 #define hl h->l 
 #define hr h->r
@@ -28,3 +28,26 @@ typedef struct _rbhead {
   kfunc    *kf ;
   rbnode_t *root ;
 } rbhead_t ;
+
+typedef struct _rbt {
+  cmpfn    *cf ;
+  ffunc    *ff ;
+  kfunc    *kf ;
+  pfunc    *pf ;
+  dfunc    *df ;
+  rbnode_t *head ;
+} rbt_t ;
+
+rbt_t  *rbt_init( cmpfn *cf, ffunc *ff, kfunc *kf, dfunc *df, pfunc *pf ) ;
+int    rbt_insert( rbt_t *bst, item_t item ) ;
+int    rbt_remove( rbt_t *rh, Key k ) ;
+item_t rbt_search( rbt_t *bst, Key k ) ;
+rbt_t  *rbt_dup( rbt_t *org, int dyn ) ;
+void   rbt_free( rbt_t *rbt ) ;
+int    rbt_items( rbt_t *rbt ) ;
+void   rbt_print( rbt_t *rbt ) ;
+
+struct _varr *rbt2varr( rbt_t *rh ) ;
+
+#endif
+
