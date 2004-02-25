5f9215362819c274fff44d1bea3d04dacdc0871a
Roland Hedberg <roland@catalogix.se>
2004-02-25 14:14:22 +0000

    function prototypes for element handling
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@74 64b275d1-5406-0410-b6a0-e042953264b1

---
+++ include/rvapi.h
@@ -0,0 +1,30 @@
+#include <struct.h>
+#include <spocp.h>
+
+int        element_size( element_t *e) ;
+int        element_type( element_t *e ) ;
+element_t *element_find_list( element_t *e, octet_t *tag) ;
+element_t *element_first( element_t *e ) ;
+element_t *element_last( element_t *e ) ;
+element_t *element_nth( element_t *e, int n ) ;
+element_t *element_next( element_t *e, element_t *ep ) ;
+void       element_reverse( element_t *e ); 
+element_t *element_copy( element_t *e ) ;
+element_t *element_parent( element_t *e ) ;
+
+void      *element_data( element_t *e ) ;
+
+element_t *element_eval( octet_t *spec, element_t *e, int *rc ) ;
+
+int        element_cmp( element_t *a, element_t *b ) ;
+
+void       element_free( element_t *e ) ;
+
+#define element_next( elem )  ((elem) ? (((element_t *)(elem))->next) : NULL ) ;
+
+element_t *element_list_add( element_t *e, element_t *a ) ;
+
+octet_t   *atoms_join( element_t *e, char *sep ) ;
+
+octet_t   *element_atom_sub( octet_t *val, element_t *ep ) ;
+
