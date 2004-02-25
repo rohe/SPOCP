0a1dbd237d9bb426659a4e43529d386c89e044c4
Roland Hedberg <roland@catalogix.se>
2004-02-25 13:41:36 +0000

    Function prototype change
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@53 64b275d1-5406-0410-b6a0-e042953264b1

--- lib/cachetime.c
+++ lib/cachetime.c
@@ -1,15 +1,16 @@
 #include <string.h>
 
 #include <plugin.h>
+#include <spocp.h>
 #include <func.h>
 #include <wrappers.h>
 
-time_t cachetime_set( octet_t *str, plugin_t *pi )
+time_t cachetime_set( octet_t *str, cachetime_t *ct )
 {
-  cachetime_t *ct, *xct = 0 ;
+  cachetime_t  *xct = 0 ;
   int          l, max = -1 ;
   
-  for( ct = pi->ct ; ct ; ct = ct->next ) {
+  for(  ; ct ; ct = ct->next ) {
     if( ct->pattern.size == 0 && max == -1 ) {
       max = 0 ;
       xct = ct ;
@@ -31,28 +32,22 @@ cachetime_t *cachetime_new( octet_t *s )
 {
   cachetime_t *new ;
   long int     li ;
-  int          n ;
-  octet_t     **arg ;
+  octarr_t    *arg ;
 
-  arg = oct_split( s, ' ', '\0', 0, 0, &n ) ;
+  arg = oct_split( s, ' ', '\0', 0, 0 ) ;
 
-  if( is_numeric( arg[0], &li ) != SPOCP_SUCCESS ) {
-    oct_freearr( arg ) ;
+  if( is_numeric( arg->arr[0], &li ) != SPOCP_SUCCESS ) {
+    octarr_free( arg ) ;
     return NULL ;
   }
 
   new = ( cachetime_t * ) Malloc ( sizeof( cachetime_t )) ;
   new->limit = ( time_t ) li ;
   
-  if( n > 0 ) {
-    new->pattern.val = arg[1]->val ;
-    new->pattern.len = arg[1]->len ;
-    new->pattern.size = arg[1]->size ;
-    arg[1]->size = 0 ; /* to protect from being freed by oct_freearr */
-  }
+  if( arg->n > 1 ) octcpy( &new->pattern, arg->arr[1] ) ; 
   else memset( &new->pattern, 0, sizeof( octet_t )) ;
 
-  oct_freearr( arg ) ;
+  octarr_free( arg ) ;
 
   return new ;
 }
