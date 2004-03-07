5862037c090f5d11b17638ae03b674c8a6c6fe0c
Roland Hedberg <roland@catalogix.se>
2004-03-07 10:34:01 +0000

    Modified the interface slightly
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@101 64b275d1-5406-0410-b6a0-e042953264b1

--- server/pool.c
+++ server/pool.c
@@ -4,16 +4,25 @@
 #include "srv.h"
 #include "wrappers.h"
 
-afpool_t *new_afpool( void )
+static pool_t *pool_new( void )
+{
+  pool_t *p ;
+
+  p = ( pool_t * ) Calloc ( 1, sizeof( pool_t )) ;
+
+  pthread_mutex_init( &p->lock, NULL ) ;
+
+  return p ;
+}
+
+afpool_t *afpool_new( void )
 {
   afpool_t *afp ;
-  pool_t   *p ;
 
   afp = ( afpool_t *) Malloc( sizeof( afpool_t )) ;
 
-  p = ( pool_t * ) Calloc ( 2, sizeof( pool_t )) ;
-  afp->free = &p[0] ;
-  afp->active = &p[1] ;
+  afp->free = pool_new() ;
+  afp->active = pool_new() ;
  
   pthread_mutex_init( &afp->aflock, NULL ) ;
 
@@ -42,8 +51,10 @@ int afpool_active_item( afpool_t *afp )
   else return 0 ;
 }
 
-void rm_from_pool( pool_t *pool, pool_item_t *item )
+void pool_rm( pool_t *pool, pool_item_t *item )
 {
+  pthread_mutex_lock( &pool->lock ) ;
+
   if( pool->head == item ) {
     if( pool->head == pool->tail ) pool->head = pool->tail = 0 ;
     else {
@@ -63,13 +74,17 @@ void rm_from_pool( pool_t *pool, pool_item_t *item )
   item->prev = item->next = 0 ;
 
   pool->size-- ;
+
+  pthread_mutex_unlock( &pool->lock ) ;
 }
  
-pool_item_t *pop_from_pool( pool_t *pool )
+pool_item_t *pool_pop( pool_t *pool )
 {
   pool_item_t *item ;
 
   if( pool->size == 0 ) return 0 ;
+
+  pthread_mutex_lock( &pool->lock ) ;
 
   item = pool->tail ;
 
@@ -84,11 +99,15 @@ pool_item_t *pop_from_pool( pool_t *pool )
 
   pool->size-- ;
 
+  pthread_mutex_unlock( &pool->lock ) ;
+
   return item ;
 }
 
-void add_to_pool( pool_t *pool, pool_item_t *item )
+void pool_add( pool_t *pool, pool_item_t *item )
 {
+  pthread_mutex_lock( &pool->lock ) ;
+
   if( pool->size == 0 ) {
     pool->tail = pool->head = item ;
     item->next = item->prev = 0 ;
@@ -103,18 +122,38 @@ void add_to_pool( pool_t *pool, pool_item_t *item )
   }
 
   pool->size++ ;
+
+  pthread_mutex_unlock( &pool->lock ) ;
+}
+
+int pool_size( pool_t *pool )
+{
+  int          i ;
+  pool_item_t *pi ;
+
+  if( pool == 0 ) return 0 ;
+ 
+  if( pool->head == 0 ) return 0 ;
+
+  pthread_mutex_lock( &pool->lock ) ;
+  
+  for( pi = pool->head, i = 0 ; pi ; pi = pi->next ) i++ ;
+
+  pthread_mutex_unlock( &pool->lock ) ;
+
+  return i ;
 }
 
 /* remove it from the active pool and place it in the free pool 
    only done from spocp_server_run(), of which there is only one running 
  */
-int return_item( afpool_t *afp, pool_item_t *item ) 
+int item_return( afpool_t *afp, pool_item_t *item ) 
 {
   if( afp == 0 ) return 0 ;
 
-  rm_from_pool( afp->active, item ) ;
+  pool_rm( afp->active, item ) ;
 
-  add_to_pool( afp->free, item ) ;
+  pool_add( afp->free, item ) ;
 
   return 1 ;
 }
@@ -122,7 +161,7 @@ int return_item( afpool_t *afp, pool_item_t *item )
 /* get a item from from the free pool, place it in the active pool
    only done from spocp_server_run(), of which there is only one running 
  */
-pool_item_t *get_item( afpool_t *afp ) 
+pool_item_t *item_get( afpool_t *afp ) 
 {
   pool_item_t *item ;
 
@@ -131,18 +170,50 @@ pool_item_t *get_item( afpool_t *afp )
   if( afp->free == 0 || afp->free->head == 0 ) return 0 ;
 
   /* get it from the free pool */
-  item = pop_from_pool( afp->free ) ;
+  item = pool_pop( afp->free ) ;
 
   /* place it in the active pool */
 
-  if( item ) add_to_pool( afp->active, item ) ;
+  if( item ) pool_add( afp->active, item ) ;
 
   return item ;
 }
 
-pool_item_t *first_active( afpool_t *afp )
+void afpool_push_item( afpool_t *afp, pool_item_t *pi )
 {
-  if( afp && afp->active && afp->active->size ) return afp->active->head ;
+  if( afp && afp->active ) pool_add( afp->active, pi ) ;
+}
+
+void afpool_push_empty( afpool_t *afp, pool_item_t *pi )
+{
+  if( afp && afp->free ) pool_add( afp->free, pi ) ;
+}
+
+pool_item_t *afpool_get_item( afpool_t *afp )
+{
+  if( afp && afp->active && afp->active->size ) return pool_pop( afp->active ) ;
   else return 0 ;
+}
+
+pool_item_t *afpool_get_empty( afpool_t *afp )
+{
+  if( afp && afp->free && afp->free->size ) return pool_pop( afp->free) ;
+  else return 0 ;
+}
+
+pool_item_t *afpool_first( afpool_t *afp )
+{
+  if( afp && afp->active ) return afp->active->head ;
+  return 0 ;
+}
+
+int number_of_active( afpool_t *afp ) 
+{
+  return pool_size( afp->active ) ;
+}
+
+int number_of_free( afpool_t *afp ) 
+{
+  return pool_size( afp->free ) ;
 }
 
