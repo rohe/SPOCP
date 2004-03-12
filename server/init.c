54ba25f4ed27156b4ace3d7109674a46f536e7a2
Roland Hedberg <roland@catalogix.se>
2004-03-12 19:40:41 +0000

    dback is sort of a plugin, but then not really
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@148 64b275d1-5406-0410-b6a0-e042953264b1

--- server/init.c
+++ server/init.c
@@ -6,6 +6,8 @@ int run_plugin_init( srv_t *srv )
   plugin_t *pl ;
 
   for( pl = srv->root->db->plugins ; pl ; pl = pl->next ) {
+    if( strcmp(pl->name,"dback") == 0 ) continue ; 
+ 
     if( pl->init ) {
       traceLog( "Running the init function for %s", pl->name ) ;
       pl->init( conf_get, srv, &pl->dyn ) ;
