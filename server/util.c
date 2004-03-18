cfbbbab352c9266e2d3164d491af87950e61ee35
Love Hörnquist Åstrand <lha@it.su.se>
2004-03-18 17:15:53 +0000

    drop unused function join_octarr
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@233 64b275d1-5406-0410-b6a0-e042953264b1

--- server/util.c
+++ server/util.c
@@ -1,31 +1,6 @@
 #include "locl.h"
 RCSID("$Id$");
 
-/* Making two arrays och octet string into one.
-   No checking for similar strings are made */
-__attribute__((unused)) static octet_t **join_octarr( octet_t **arr0, octet_t **arr1 )
-{
-  int  i, j, n, m ;
-  octet_t **res ;
-
-  if( arr0 == 0 ) return arr1 ;
-  if( arr1 == 0 ) return arr0 ;
-  if( arr1 == 0 && arr0 == 0 ) return 0 ;
-
-  for( n = 0 ; arr0[n]  ; n++ ) ;
-  for( m = 0 ; arr1[m]  ; m++ ) ;
-
-  res = ( octet_t ** ) realloc ( arr0, n+m+1 * sizeof( octet_t * )) ; 
-
-  for( i = n, j = 0 ; j < m ; j++, i++ ) 
-    res[i] = arr1[j] ; 
-
-  res[i] = 0 ;
-  free( arr1 ) ;
-
-  return res ;
-}
-
 char *rm_lt_sp( char *s, int shift )
 {
   char *sp, *cp ;
