aea7be90561dcd78283694cf4da39fe89493c438
Roland Hedberg <roland@catalogix.se>
2004-04-28 10:58:31 +0000

    new file
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@307 64b275d1-5406-0410-b6a0-e042953264b1

---
+++ server/opstack.c
@@ -0,0 +1,52 @@
+#include <locl.h>
+
+opstack_t *
+opstack_new( int op, ruleset_t *rs, octarr_t *arg)
+{
+	opstack_t *os;
+	
+	os = (opstack_t *)Calloc(1, sizeof( opstack_t));
+
+	os->oper = op;
+	os->rs = rs;
+	os->oparg = octarr_dup(arg);
+	os->rollback = 0;
+	return os;
+}
+
+void
+opstack_free( opstack_t *osn )
+{
+	if (osn) {
+		if (osn->oparg)
+			octarr_free( osn->oparg );
+		if (osn->rollback)
+			octarr_free( osn->rollback);
+		if (osn->next)
+			opstack_free( osn->next );
+
+		free( osn );
+	}
+}
+
+opstack_t *
+opstack_push( opstack_t *a, opstack_t *b)
+{
+	if (a) {
+		while (a->next) a = a->next;
+
+		a->next = b;
+		return a;
+	}
+	else
+		return b;
+}
+
+/*
+spocp_result_t
+opstack_commit( opstack_t *head )
+{
+	
+}
+*/
+
