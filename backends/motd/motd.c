e3dddd36a77b68facb48755d313de2fd3ae701e2
Roland Hedberg <roland@catalogix.se>
2004-04-29 11:47:12 +0000

    Extended to allow testing of plugin directives
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@319 64b275d1-5406-0410-b6a0-e042953264b1

--- backends/motd/motd.c
+++ backends/motd/motd.c
@@ -20,19 +20,34 @@
 #include <be.h>
 #include <plugin.h>
 #include <rvapi.h>
+#include <macros.h>
 
-#define DIGITS(n) ( (n) >= 100000 ? 6 : (n) >= 10000 ? 5 : (n) >= 1000 ? 4 : (n) >= 100 ? 3 : ( (n) >= 10 ? 2 : 1 ) )
+befunc	motd_test;
+conf_args set_name;
 
-befunc          motd_test;
+char	*motd = "/etc/motd";
+char	*type = "Text/plain";
 
-char           *motd = "/etc/motd";
-char           *type = "Text/plain";
+/* ====================================================================== */
+
+spocp_result_t
+set_name( void **lh, void *cd, int argc, char **argv)
+{
+	if (argc >= 1) {
+		*lh = strdup(argv[0]);
+		return SPOCP_SUCCESS;
+	}
+	else
+		return SPOCP_MISSING_ARG;
+}
+
+/* ---------------------------------------------------------------------- */
 
 spocp_result_t
 motd_test(cmd_param_t * cpp, octet_t * blob)
 {
 	FILE           *fp;
-	char            buf[256], *lp;
+	char            buf[256], *lp, *txt = 0;
 	int             len, lt, tot;
 
 	fp = fopen(motd, "r");
@@ -51,8 +66,20 @@ motd_test(cmd_param_t * cpp, octet_t * blob)
 	while (lp != buf && (*lp == '\r' || *lp == '\n'))
 		*lp-- = '\0';
 
-	len = strlen(buf);
 	lt = strlen(type);
+
+	if (cpp->conf){
+		char *cp = (char *) cpp->conf;
+
+		len = strlen(buf);
+		txt = (char *) malloc( lt + len + strlen(cp) + 9);
+
+		sprintf( txt, "%s says \"%s\"", cp, buf);
+	} 
+	else
+		txt = buf;
+
+	len = strlen(txt);
 
 	if (len == 0)
 		return SPOCP_UNAVAILABLE;
@@ -61,15 +88,22 @@ motd_test(cmd_param_t * cpp, octet_t * blob)
 	if (blob) {
 		blob->len = blob->size = tot;
 		blob->val = (char *) malloc(tot * sizeof(char));
-		sprintf(blob->val, "%d:%s%d:%s", lt, type, len, buf);
+		sprintf(blob->val, "%d:%s%d:%s", lt, type, len, txt);
 	}
+
+	if (txt != buf) free(txt);
 
 	return SPOCP_SUCCESS;
 }
+
+conf_com_t conffunc[] = {
+	{ "who", set_name, NULL, "The message guy" },
+	{NULL}
+};
 
 plugin_t        motd_module = {
 	SPOCP20_PLUGIN_STUFF,
 	motd_test,
 	NULL,
-	NULL
+	conffunc
 };
