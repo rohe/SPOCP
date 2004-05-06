67d170106b2f674173a85795eedaeebf76ecca1c
Roland Hedberg <roland@catalogix.se>
2004-05-06 12:46:20 +0000

    New server for admin use
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@333 64b275d1-5406-0410-b6a0-e042953264b1

---
+++ server/adminsrv.c
@@ -0,0 +1,337 @@
+#include <stdio.h>
+
+#include <spocp.h>
+#include <struct.h>
+#include <locl.h>
+
+#define  SPOCP_NEW(X,y) ((X) = Calloc(y, sizeof(*(X))))
+
+typedef struct _rule {
+	octet_t		*path;
+	octet_t		*rule;
+	octet_t		*blob;
+	char		*strrule;
+	element_t	*ep;
+	bcdef_t		*bcond;
+	struct _rule	*next;
+	struct _rule	*prev;
+} rule_t ;
+
+srv_t	srv;
+dbcmd_t	*dbc = 0;
+
+int 	debug = 0;
+int	extended = 0;
+
+int elementcmp( element_t *a, element_t *b, octet_t *str );
+
+/* ---------------------------------------------------------------------- */
+
+static rule_t
+*rule_new( octet_t *path )
+{
+	rule_t *r;
+
+	r = ( rule_t * ) Calloc ( 1, sizeof( rule_t ));
+	if( path ) r->path = octdup( path );
+
+	return r;
+}
+
+static void
+rule_free( rule_t *r )
+{
+	if (r) {
+		if (r->path) oct_free( r->path );
+		if (r->rule) oct_free( r->rule );
+		if (r->blob) oct_free( r->blob );
+		if (r->ep) element_free( r->ep);
+		free(r);
+	}
+}
+
+/* ---------------------------------------------------------------------- */
+
+static int
+as_read_rules( char *file, rule_t **rule)
+{
+	FILE           *fp;
+	char           *sp, *tmp;
+	int             n = 0, f = 0;
+	octet_t         oct, *op;
+	spocp_result_t  rc = SPOCP_SUCCESS;
+	spocp_charbuf_t	buf;
+	spocp_chunk_t	*chunk = 0, *ck;
+	spocp_ruledef_t	rdef;
+	rule_t		*top = 0, *nr;
+	element_t	*ep;
+	bcdef_t		*bcd;
+				
+
+	if ((fp = fopen(file, "r")) == 0) {
+		LOG(SPOCP_EMERG) traceLog("couldn't open rule file \"%s\"",
+					  file);
+		sp = getcwd(oct.val, oct.size);
+		LOG(SPOCP_EMERG) traceLog("I'm in \"%s\"", sp);
+		free(oct.val);
+		return -1;
+	}
+
+	/*
+	 * The default ruleset should already be set 
+	 */
+
+	buf.fp = fp;
+	buf.str = (char *) calloc ( BUFSIZ, sizeof( char ));
+	buf.size = BUFSIZ;
+	buf.start = buf.str ; 
+
+	if (get_more(&buf) == 0) return 0;
+
+	/*
+	 * have to escape CR since fgets stops reading when it hits a newline
+	 * NUL also has to be escaped since I have problem otherwise finding
+	 * the length of the 'string'. '\' hex hex is probably going to be the 
+	 * choice 
+	 */
+	while (rc == SPOCP_SUCCESS && ( chunk = get_object( &buf, 0 )) != 0 ) {
+		if (oct2strcmp(chunk->val, ";include ") == 0) {	/* include
+								 * file */
+			ck = chunk->next;
+			tmp = oct2strdup( ck->val, 0 ) ;
+			LOG(SPOCP_DEBUG) traceLog("include directive \"%s\"",
+						  tmp);
+			if ((rc = as_read_rules(tmp, rule)) < 0) {
+				traceLog("Include problem");
+			}
+		}
+		else if (*chunk->val->val == '/' || *chunk->val->val == '(') {
+
+
+			if (*chunk->val->val == '/') {
+				nr = rule_new( chunk->val ) ;
+				ck = chunk->next;
+			}
+			else  {
+				nr = rule_new( NULL );
+				ck = chunk;
+			}
+
+			ruledef_return( &rdef, ck ) ;
+			if( rdef.rule ) {
+				octet_t loc;
+
+				op = chunk2sexp( rdef.rule ) ;
+				octln( &loc, op);
+				rc = element_get(&loc, &ep);
+
+				if (rc != SPOCP_SUCCESS) {
+					chunk_free( chunk );
+					rule_free( nr );
+					continue;
+				}
+
+				nr->rule = op;
+				nr->strrule = oct2strdup( op, '%' );
+				nr->ep = ep;
+			}
+			
+			if( rdef.bcond) {
+				op = chunk2sexp( rdef.bcond ) ;
+				bcd = bcdef_get(srv.root->db, srv.plugin, dbc, op, &rc);
+				if (bcd == NULL && rc != SPOCP_SUCCESS) {
+					chunk_free( chunk );
+					rule_free( nr );
+					continue;
+				}
+				nr->bcond = bcd;
+			}
+			
+			if( rdef.blob )
+				nr->blob = octdup(rdef.blob->val) ;
+			
+			nr->next = top;
+			if (top)
+				top->prev = nr;
+
+			top = nr;
+			n++;
+		}
+       		else {
+			octet_t *key, *val, *to = 0;
+
+			key = chunk->val ;
+			if( *chunk->next->next->val->val == '(' ) {
+				ruledef_return( &rdef, chunk->next->next ) ;
+				if( rdef.rule) {
+					val = to = chunk2sexp( rdef.rule ) ;
+				}
+				else {
+					printf("Error in bconddef\n");
+					f++;
+					chunk_free( chunk ) ;
+					continue ;
+				}
+			}
+			else 
+				val = chunk->next->next->val ;
+
+			if (bcdef_add(srv.root->db, srv.plugin, dbc, key, val) == 0 )
+				rc = SPOCP_LOCAL_ERROR;
+
+			if (to) oct_free( to );
+		}
+
+		chunk_free( chunk ) ;
+	}
+
+	if (rc != SPOCP_SUCCESS)
+		return -1;
+	else {
+		LOG(SPOCP_INFO) traceLog("Stored %d rules, failed %d", n, f);
+		*rule = top;
+		return n;
+	}
+}
+
+/* ---------------------------------------------------------------------- */
+
+int main( int argc, char **argv )
+{
+	element_t	*ep = 0;
+	int		n;
+	rule_t		*top = 0, *r;
+	spocp_charbuf_t scb;
+	spocp_chunk_t	*sc = 0;
+	octet_t		*op,loc, *ext = 0;
+	spocp_result_t	rc, sr;
+	char 		*cnfg = 0, str[1024];
+	int		i, runbc = 0, m;
+	octarr_t	*info = 0;
+
+	memset( &srv, 0, sizeof( srv_t ));
+
+	while ((i = getopt(argc, argv, "hbxf:d:")) != EOF) {
+		switch (i) {
+
+		case 'f':
+			cnfg = strdup(optarg);
+			break;
+
+		case 'd':
+			debug = atoi(optarg);
+			if (debug < 0)
+				debug = 0;
+			break;
+
+		case 'b':
+			runbc = 1;
+			break;
+
+		case 'x':
+			extended = 1;
+			break;
+
+		case 'h':
+		default:
+			fprintf(stderr,
+				"Usage: %s [-f configfile] [-d debuglevel]\n",
+				argv[0]);
+			exit(0);
+		}
+	}
+
+	if (srv.root == 0) 
+/*
+		srv.root = (ruleset_t *)Calloc(1, sizeof(ruleset_t));
+*/
+		SPOCP_NEW( srv.root, 1);
+
+	if (cnfg == 0 || read_config(cnfg, &srv) == 0) {
+		printf( "Error reading rules from %s\n", cnfg);
+		exit(1) ;
+	}
+
+	if (srv.rulefile == 0 || (n = as_read_rules( srv.rulefile, &top)) < 0 ) {
+		printf( "Error reading rules from %s\n", srv.rulefile);
+		exit(1) ;
+	}
+
+	if (n == 0) {
+		printf( "Didn't find any rules to use\n");
+		exit(0);
+	}
+
+	printf( "Read %d rules\n", n );
+	
+	if (optind == argc) {
+		scb.fp = stdin;
+		scb.str = (char *) calloc ( BUFSIZ, sizeof( char ));
+		scb.size = BUFSIZ;
+		scb.start = scb.str ; 
+
+		if (get_more(&scb) == 0) return 0;
+	}
+	else {
+		scb.str = argv[optind];
+		scb.start = scb.str;
+		scb.size = 0;
+		scb.fp = 0;
+	}
+
+	if (extended) 
+		ext = oct_new( 1024, 0);
+
+	while ((sc = get_object( &scb, 0 )) != NULL) {
+		op = chunk2sexp( sc ) ;
+		oct2strcpy( op, str, 1024, '%');
+		printf("%s", str);
+
+		if (extended)
+			printf("\n");
+
+		octln( &loc, op);
+		if(( rc = element_get(&loc, &ep)) == SPOCP_SUCCESS) {
+			for ( r = top, m = 0 ; r ; r = r->next ) { 
+				if (extended) {
+					*ext->val = '\0';
+					ext->len = 0;
+				}
+	
+				if ( elementcmp( ep, r->ep, ext ) == 0 ) {
+					printf( " YES [%s", r->strrule);
+					if (runbc) {
+						sr = bcexp_eval(ep, r->ep,
+						    r->bcond->exp, &info);
+						if (sr==SPOCP_SUCCESS)
+							printf(", bcond: OK\n");
+						else if (sr==SPOCP_DENIED)
+							printf(", bcond: DENIED\n");
+						else
+							printf(", bcond: %d\n",sr);
+					}
+					else
+						printf("]\n");
+					m++;
+				}
+				else if( extended ) {
+					oct2strcpy( ext, str, 1024, '%' );
+					printf("\t[%s] of [%s]\n",
+					   str, r->strrule);
+				}
+			}
+
+			if (!m)
+				printf( " NO\n" );
+
+			element_free( ep );
+		}
+		oct_free( op );
+		chunk_free( sc );
+	}
+
+
+	exit(0);
+}
+
+
---
+++ server/cmp.c
@@ -0,0 +1,282 @@
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#include <spocp.h>
+#include <struct.h>
+#include <varr.h>
+
+/*
+ * verify 
+ */
+
+spocp_result_t  is_numeric(octet_t * op, long *l);
+spocp_result_t  numstr(char *s, long *l);
+spocp_result_t  is_ipv4(octet_t * op, struct in_addr *ia);
+spocp_result_t  is_ipv6(octet_t * op, struct in6_addr *ia);
+spocp_result_t  is_date(octet_t * op);
+spocp_result_t  is_time(octet_t * op);
+spocp_result_t  is_extref(octet_t * op);
+spocp_result_t  is_ipv4_s(char *ip, struct in_addr *ia);
+spocp_result_t  is_ipv6_s(char *ip, struct in6_addr *ia);
+
+/*
+ *
+ */
+
+int             ipv4cmp(struct in_addr *ia1, struct in_addr *ia2);
+int             ipv6cmp(struct in6_addr *ia1, struct in6_addr *ia2);
+spocp_result_t  element_get(octet_t * oct, element_t ** epp);
+
+extern int debug;
+
+/*
+ *
+ */
+
+int elementcmp( element_t *a, element_t *b, octet_t *str );
+
+/*
+ * ----------------------------------------------------------------------
+ */
+
+static int
+atomcmp( atom_t *a, atom_t *b, octet_t *str )
+{
+	char *c,*s;
+	int r = octcmp( &a->val, &b->val );
+	
+	if (debug) {
+		c = oct2strdup( &a->val, '%' );
+		s = oct2strdup( &b->val, '%' );
+	
+		printf("atomcmp: [%s][%s] => %d\n", c,s,r);
+
+		free(c);
+		free(s);
+	}
+	if( str && r == 0 ) octcat( str, a->val.val, a->val.len );
+
+	return r;
+}
+
+static int
+atom2boundarycmp( atom_t *a, boundary_t *b, int *dir, octet_t *str)
+{
+	int		res = TRUE, v = 0;
+	struct in_addr	ia4;
+	struct in6_addr	ia6;
+	long		l;
+	
+	switch (b->type & 0x07) {
+	case SPOC_ALPHA:
+		v = octcmp(&a->val, &b->v.val);
+		break;
+
+	case SPOC_DATE:
+		if (is_date( &a->val) == SPOCP_SUCCESS) {
+			v = octcmp(&a->val, &b->v.val);
+			res = TRUE;
+		}
+		break;
+
+	case SPOC_TIME:
+		if (is_time( &a->val) == SPOCP_SUCCESS) {
+			if (is_numeric( &a->val, &l) == SPOCP_SUCCESS)
+				v = l - b->v.num;
+
+			res = TRUE;
+		}
+		break;
+
+	case SPOC_NUMERIC:
+		if (is_numeric( &a->val, &l) == SPOCP_SUCCESS) {
+			v = l - b->v.num;
+			res = TRUE;
+		}
+		break;
+
+	case SPOC_IPV4:
+		if (is_ipv4( &a->val, &ia4) == SPOCP_SUCCESS ) {
+			v = ipv4cmp(&ia4, &b->v.v4);
+			res = TRUE;
+		}
+		break;
+
+	case SPOC_IPV6:	/* Can be viewed as a array of unsigned ints */
+		if (is_ipv6( &a->val, &ia6) == SPOCP_SUCCESS) {
+			v = ipv6cmp(&ia6, &b->v.v6);
+			res = TRUE;
+		}
+		break;
+
+	default:
+		res = FALSE;
+		break;
+	}
+
+	if( res == TRUE )
+		*dir = v;
+
+	return res;
+}
+
+static int
+atom2rangecmp( atom_t *a, range_t *r, octet_t *str )
+{
+	int d;
+
+	if ( atom2boundarycmp( a, &r->lower, &d, str ) == FALSE ) 
+		return FALSE;
+	else if ( r->lower.type == (GLE|GT) && d >= 0 ) ;
+	else if ( r->lower.type == GT && d > 0 ) ;
+	else 
+		return FALSE;	
+
+	if ( atom2boundarycmp( a, &r->upper, &d, str ) == FALSE ) 
+		return FALSE;
+	else if ( r->lower.type == (GLE|LT) && d <= 0 ) 
+		return TRUE;
+	else if ( r->lower.type == GT && d > 0 ) 
+		return TRUE;
+	else 
+		return FALSE;	
+}
+
+/* anything but 0 is failure */
+static int
+atom2prefixcmp( atom_t *a, atom_t *s, octet_t *str)
+{
+	if( a->val.len >= s->val.len )
+		return octncmp( &a->val, &s->val, s->val.len);
+	else
+		return 1;
+}
+
+static int
+atom2suffixcmp( atom_t *a, atom_t *s, octet_t *str)
+{
+	octet_t tmp;
+	size_t n;
+
+	if( a->val.len >= s->val.len ) {
+		n = s->val.len - a->val.len;
+		tmp.val = s->val.val + n;
+		tmp.len = s->val.len - n;
+		return octcmp( &tmp, &s->val);	
+	}
+	else
+		return 1;
+}
+
+static int
+suffixcmp( atom_t *a, atom_t *b, octet_t *str )
+{
+	return octcmp( &a->val, &b->val );
+}
+
+static int
+prefixcmp( atom_t *a, atom_t *b, octet_t *str )
+{
+	return octcmp( &a->val, &b->val );
+}
+
+static int
+rangecmp( range_t *a, range_t *b, octet_t *str )
+{
+	return 1;
+}
+
+static int
+element2setcmp( element_t *a, varr_t *s, octet_t *str)
+{
+	int i;
+
+	for( i = 0 ; i < (int) s->n ; i++ )
+		if (elementcmp( a, s->arr[i], str) == 0 ) return 0;
+
+	return 1;	
+}
+
+static int
+listcmp( list_t *la, list_t *lb, octet_t *str )
+{
+	element_t *a, *b;
+
+	if (str) 
+		octcat( str, "(", 1 );
+
+	for( a = la->head, b = lb->head; a && b ; a = a->next, b = b->next) 
+		if (elementcmp( a, b, str ) != 0) return 1;
+
+	/* The b list is longer ? */
+	if (b != NULL) return 1; 
+
+	if (str) 
+		octcat( str, ")", 1 );
+
+	return 0;
+}
+
+int
+elementcmp( element_t *a, element_t *b, octet_t *str )
+{
+	switch (a->type) {
+	case SPOC_ATOM:
+		switch ( b->type ) {
+		case SPOC_ATOM: 
+			return atomcmp( a->e.atom, b->e.atom, str);	
+
+		case SPOC_PREFIX:
+			return atom2prefixcmp( a->e.atom, b->e.atom, str);
+			
+		case SPOC_SUFFIX:
+			return atom2suffixcmp( a->e.atom, b->e.atom, str);
+
+		case SPOC_RANGE:
+			return atom2rangecmp( a->e.atom, b->e.range, str);
+
+		case SPOC_SET:
+			return element2setcmp( a, b->e.set, str );
+
+		default:
+			return 1;
+
+		}
+		break;
+
+	case SPOC_PREFIX:
+		if (b->type == SPOC_PREFIX)
+			return prefixcmp( a->e.atom, b->e.atom, str);
+		break;
+
+	case SPOC_SUFFIX:
+		if (b->type == SPOC_SUFFIX)
+			return suffixcmp( a->e.atom, b->e.atom, str);
+		break;
+
+	case SPOC_RANGE:
+		if (b->type == SPOC_RANGE)
+			return rangecmp( a->e.range, b->e.range, str);
+		else if( b->type == SPOC_SET )
+			return element2setcmp( a, b->e.set, str );
+		break;
+
+	case SPOC_LIST:
+		if( b->type == SPOC_LIST)
+			return listcmp( a->e.list, b->e.list, str);
+		break;
+	} 
+
+	return 1;
+}
+
+/*
+static int
+list2setcmp( list_t *l, varr_t *s )
+{
+	return 0;
+}
+*/
