eaae6862c42708cda40663e34fe0ca801966b867
Klas Lindfors <kllin@it.su.se>
2007-03-05 16:24:20 +0000

    forgotten code for spocp_chunkwrap_t from roland.
    git-svn-id: svn+ssh://svn.it.su.se/svn/spocp/trunk@629 64b275d1-5406-0410-b6a0-e042953264b1

--- server/readcnf.h
+++ server/readcnf.h
@@ -11,7 +11,12 @@ typedef struct _ruledef {
 	spocp_chunk_t	*blob;
 } spocp_ruledef_t;
 
-spocp_chunk_t	*get_object(spocp_charbuf_t *, spocp_chunk_t *);
+typedef struct _chunkwrap {
+    spocp_chunk_t   *chunk;
+    int             status;
+} spocp_chunkwrap_t;
+
+spocp_chunkwrap_t	*get_object(spocp_charbuf_t *, spocp_chunk_t *);
 void		ruledef_return(spocp_ruledef_t *, spocp_chunk_t *);
 
 #endif
