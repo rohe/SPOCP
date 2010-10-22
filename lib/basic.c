0f053faf48d438b29f20ee89ff58b8a87d0c9bab
Roland Hedberg <roland.hedberg@adm.umu.se>
2010-10-22 14:47:02 +0200

    Started to run splint on code

--- lib/basic.c
+++ lib/basic.c
@@ -44,20 +44,20 @@
 int
 ipv4cmp(struct in_addr *ia1, struct in_addr *ia2)
 {
-    return ia1->s_addr - ia2->s_addr;
+    return (int) (ia1->s_addr - ia2->s_addr);
 }
 
 #ifdef USE_IPV6
 int
 ipv6cmp(struct in6_addr *ia1, struct in6_addr *ia2)
 {
-    uint8_t     *la = &(ia1->s6_addr[15]) ;
-    uint8_t     *lb = &(ia2->s6_addr[15]) ;
-    int             r, i;
+    __uint8_t   *la = &(ia1->s6_addr[15]) ;
+    __uint8_t   *lb = &(ia2->s6_addr[15]) ;
+    int         r, i;
 
     for (i = 15; i >= 0; i++, la--, lb--) {
-        r = *la - *lb;
-        if (r)
+        r = (int) (*la - *lb);
+        if (r != 0)
             return r;
     }
 
@@ -81,7 +81,8 @@ to_gmt(octet_t * s, octet_t * t)
     time_t          tid;
 
     if (s->len == 19 || s->len == 20) {
-        octcpy(t, s);
+        if(octcpy(t, s) != SPOCP_SUCCESS)
+            return ;
         t->len = 19;
     } else {        /* with offset, will be at least 21 characters long */
         /* Get me the time without offset */
@@ -94,31 +95,32 @@ to_gmt(octet_t * s, octet_t * t)
         sp = s->val + 19;
         if (*sp == '+') {
             sp++;
-            tid += (*sp++ - '0') * 36000;
-            tid += (*sp++ - '0') * 3600;
+            tid += (time_t)((int)(*sp++ - '0') * 36000);
+            tid += (time_t)((int)(*sp++ - '0') * 3600);
             if (s->len > 22) {
                 sp++;
-                tid += (*sp++ - '0') * 600;
-                tid += (*sp++ - '0') * 60;
+                tid += (time_t)((int)(*sp++ - '0') * 600);
+                tid += (time_t)((int)(*sp++ - '0') * 60);
             }
         } else {
             sp++;
-            tid -= (*sp++ - '0') * 36000;
-            tid -= (*sp++ - '0') * 3600;
+            tid -= (time_t)((int)(*sp++ - '0') * 36000);
+            tid -= (time_t)((int)(*sp++ - '0') * 3600);
             if (s->len > 22) {
                 sp++;
-                tid -= (*sp++ - '0') * 600;
-                tid -= (*sp++ - '0') * 60;
+                tid -= (time_t)((int)(*sp++ - '0') * 600);
+                tid -= (time_t)((int)(*sp++ - '0') * 60);
             }
         }
 
         gmtime_r(&tid, &tm);
 
-        t->val = (char *) Malloc(20 * sizeof(char));
-        t->val[19] = 0;
+        sp = (char *) Malloc(20 * sizeof(char));
+        sp[19] = '\0';
 
-        strftime(t->val, 20, "%Y-%m-%dT%H:%M:%S", &tm);
-        t->len = 19;
+        memset(&tm, 0, sizeof(struct tm));
+        (void) strftime(sp, 20, "%Y-%m-%dT%H:%M:%S", &tm);
+        octset(t, sp, (int) strlen(sp));
     }
 }
 
@@ -130,18 +132,18 @@ hms2int(octet_t * op, long *num)
     cp = op->val;
 
     *num = 0;
-    *num += (*cp++ - '0') * 36000;
-    *num += (*cp++ - '0') * 3600;
+    *num += (time_t)((int)(*cp++ - '0') * 36000);
+    *num += (time_t)((int)(*cp++ - '0') * 3600);
 
     cp++;
 
-    *num += (*cp++ - '0') * 600;
-    *num += (*cp++ - '0') * 60;
+    *num += (time_t)((int)(*cp++ - '0') * 600);
+    *num += (time_t)((int)(*cp++ - '0') * 60);
 
     cp++;
 
-    *num += (*cp++ - '0') * 10;
-    *num += (*cp++ - '0');
+    *num += (time_t)((int)(*cp++ - '0') * 10);
+    *num += (time_t)((int)(*cp++ - '0'));
 }
 
 /*
